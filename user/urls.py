bb85d6eaf45f8becb49849339a3d1e6cc6a2c707
Gabriel Ribeiro <gabrielgoncalves310503@gmail.com>
2025-05-30 14:27:53 -0300

    models adjusts

--- user/urls.py
+++ user/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from user.views import CreateUserView, UpdateUserView
 
 urlpatterns = [
-    path("users/", CreateUserView.as_view(), name="create-user"),
-    path("users/<int:user_id>/", UpdateUserView.as_view(), name="update-user"),
+    path("profile/", CreateUserView.as_view(), name="create-user"),
+    path("profile/<int:user_id>/", UpdateUserView.as_view(), name="update-user"),
 ]
