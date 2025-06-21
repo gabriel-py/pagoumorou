93c6348a560b80bebc667fe143265b93c9d15128
Gabriel Ribeiro <gabrielgoncalves310503@gmail.com>
2025-06-21 13:52:55 -0300

    adjust models

--- pagoumorou/models.py
+++ pagoumorou/models.py
@@ -54,16 +54,22 @@ class Room(models.Model):
     room_number = models.CharField(max_length=50)
     capacity = models.IntegerField()
     shared = models.BooleanField(default=False)
-    weekly_price = models.DecimalField(max_digits=10, decimal_places=2)
-    biweekly_price = models.DecimalField(max_digits=10, decimal_places=2)
-    monthly_price = models.DecimalField(max_digits=10, decimal_places=2)
     property = models.ForeignKey(Property, on_delete=models.CASCADE)
+    accept_men = models.BooleanField(default=True)
+    accept_women = models.BooleanField(default=True)
 
     def __str__(self):
         return f"Room {self.room_number} - {self.property.name}"
 
     class Meta:
         db_table = "room"
+
+
+class RoomPrice(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    period = models.CharField(max_length=10, choices=PeriodChoices.choices, default=PeriodChoices.SEMESTER)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
 
 class Feature(models.Model):
     name = models.CharField(max_length=100)
